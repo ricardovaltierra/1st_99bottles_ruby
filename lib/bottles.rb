4201772b26c9215b42fd450032a2675e02184f2d
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-13 19:11:20 -0600

    Refactor code to make pass test_verse_0

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,24 +3,32 @@ class Bottles
     last_bottles = bottles - 1
 
     <<-VERSE
-#{pluralize bottles} of beer on the wall, #{pluralize bottles} of beer.
-Take #{pluralize_last bottles} and pass it around, #{pluralize last_bottles} of beer on the wall.
+#{pluralize bottles, true} of beer on the wall, #{pluralize bottles} of beer.
+#{pluralize_last bottles}, #{pluralize last_bottles} of beer on the wall.
 VERSE
   end
 
   private
 
-  def pluralize(bottles)
+  def pluralize(bottles, first_time = false)
     if bottles >= 2
       return "#{bottles} bottles"
     elsif bottles == 1
       return "#{bottles} bottle"
+    elsif bottles == 0
+      return "#{first_time ? 'N' : 'n'}o more bottles"
     end
 
-    "no more bottles"
+    "99 bottles"
   end
 
   def pluralize_last(bottles)
-    "#{bottles <= 1 ? 'it' : 'one'} down"
+    if bottles >= 2
+      return "Take one down and pass it around"
+    elsif bottles == 1
+      return "Take it down and pass it around"
+    end
+    
+    "Go to the store and buy some more"
   end
 end
